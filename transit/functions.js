d4070af54dd4b5b1a6292dcd790d7dbd74fcef37
obaidf <ofarooqui12crh@gmail.com>
2014-03-12 16:16:44 -0400

    markers

--- transit/functions.js
+++ transit/functions.js
@@ -23,14 +23,23 @@ function startTheMap()
 					};
 
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-//	me = new google.maps.LatLng(lat, lng);
 
-	//map.panTo(me);
+
+
+	//map.panTo(me); //why this is causing an infinite loop?
 
 				// Create a marker
-				marker = new google.maps.Marker({
+				
+					marker = new google.maps.Marker({
 					position: me,
 					title: "Here I Am!"
 				});
 				marker.setMap(map);
+
+				// Open info window on click of marker
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(marker.title);
+					infowindow.open(map, marker);
+				});
+
 }
