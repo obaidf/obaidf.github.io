ea2e984941d65144f99be692e431197f758ddd25
obaidf <ofarooqui12crh@gmail.com>
2014-03-12 14:57:44 -0400

    map or not?
    Get a quick map up

---
+++ transit/functions.js
@@ -0,0 +1,22 @@
+function drawMap()
+{
+	var lat;
+	var lng;
+	if (navigator.geolocation) { 
+					navigator.geolocation.getCurrentPosition(function(position) {
+						lat = position.coords.latitude;
+						lng = position.coords.longitude;
+						renderMap();
+					};
+				}
+	else {
+		alert("Geolocation is not supported by your web browser. Please update or use a different browser.");
+		lat = 0;
+		lng = 0;
+	}
+	
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	me = new google.maps.LatLng(lat, lng);
+
+
+}
