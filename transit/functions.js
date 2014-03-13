38b5d0de1552b76ef486d1ad03b04d83b0020e7f
obaidf <ofarooqui12crh@gmail.com>
2014-03-12 23:12:09 -0400

    xhml request added

--- transit/functions.js
+++ transit/functions.js
@@ -13,15 +13,27 @@ function startTheMap()
 			var marker;
 			var infowindow = new google.maps.InfoWindow();
 			var places;
+			var xhr;
+
 			init();
+
+			xhr = new XMLHttpRequest();
+	//xhr.open("method", "url", asynchronous? true or false);
+	xhr.open("GET", "http://mbta.herokuapp.com/mapper/rodeo.json", true);
+	//onreadystatechange has to be set to a function when request is completed to handle the response
+	xhr.onreadystatechange = dataReady();
+	xhr.send(null); //Go! Execute!
+
+	console.log("Yay!!!")
+
 }
-			function init()
+function init()
 			{
 				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 				getMyLocation();
 			}
 
-			function getMyLocation()
+function getMyLocation()
 			{
 				if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
 					navigator.geolocation.getCurrentPosition(function(position) {
@@ -35,7 +47,7 @@ function startTheMap()
 				}
 			}
 
-			function renderMap()
+function renderMap()
 			{
 				me = new google.maps.LatLng(myLat, myLng);
 
@@ -65,7 +77,7 @@ function startTheMap()
 				//service.search(request, callback); */
 			}
 
-			function createMarker(place)
+function createMarker(place)
 			{
 				var placeLoc = place.geometry.location;
 				var marker = new google.maps.Marker({
@@ -79,6 +91,30 @@ function startTheMap()
 					infowindow.open(map, this);
 				});
       		}
+
+//src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=TRUE";
+/*function initialize(){
+	var mapOptions = {
+		zoom: 8
+	};
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);*/
+
+	
+
+
+}
+
+function dataReady(){
+	if (xhr.readystate==4 && xhr.status == 200){
+	console.log("DONE!")
+	scheduleData = JSON.parse(xhr.responseText);
+	console.log(schedule);
+	scheduleDom = document.getElementbyId("map-canvas");
+	scheduleDom.innerHTML = scheduleData["line"];}
+	//for the assigment can get away with 500 status code else do an "else statement" to catch this error}
+
+
+
 
 
 
