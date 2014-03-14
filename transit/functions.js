a32f59f1047e2d579eae15ea9a811508522d3717
obaidf <ofarooqui12crh@gmail.com>
2014-03-13 20:32:11 -0400

    just one

--- transit/functions.js
+++ transit/functions.js
@@ -202,19 +202,16 @@ for (i = 0; i < all_stations.length; i++) {
 
 
 
-			for (i = 0; i < all_stations.length; i++) {
-				//draw_station(all_stations[i]);
-				var station_loc = new google.maps.LatLng(all_stations[i]['Lat'],all_stations[i]['Lng']);
-				var marker = new google.maps.Marker({
+				var marker1 = new google.maps.Marker({
 					//map: map,
 					position: new google.maps.LatLng(42, -71),//(all_stations[i]['Lat'],all_stations[i]['Lng']),
 					title: "asddfgg"
 					//icon:'./T_marker.png'
 				});
-				marker.setMap(map);
+				marker1.setMap(map);
 
 			
-		}
+		
 
 
 				// Open info window on click of marker
