c01636c71e099f85f8d4ec2dbc3b095358932a16
obaidf <ofarooqui12crh@gmail.com>
2014-03-13 20:42:00 -0400

    leave in map: map

--- transit/functions.js
+++ transit/functions.js
@@ -166,13 +166,13 @@ function startTheMap()
 
 
 						for (i = 0; i < all_stations.length; i++) {
-			if (all_stations[i]['Line'] == color) {
-				//draw_station(all_stations[i]);
-				var station_loc = new google.maps.LatLng(all_stations[i]['Lat'],all_stations[i]['Lng']);
-				var marker = new google.maps.Marker({
-					//map: map,
-					position: station_loc
-					//icon:'./T_marker.png'
+							if (all_stations[i]['Line'] == color) {
+							//draw_station(all_stations[i]);
+							var station_loc = new google.maps.LatLng(all_stations[i]['Lat'],all_stations[i]['Lng']);
+							var marker = new google.maps.Marker({
+							map: map,
+							position: station_loc
+							//icon:'./T_marker.png'
 				});
 				marker.setMap(map);
 
